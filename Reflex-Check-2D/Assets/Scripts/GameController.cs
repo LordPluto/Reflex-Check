849c5467dfb9e34afd88315e1a9ba668c5caf9e4
Shannon Weir <stw3763@rit.edu>
2015-01-28 21:20:32 -0500

    Object Guessing
    Objects now randomly selected from a group of ten (or more, but at the
    moment only ten objects exist). Guessing the correct object works.
    
    + Object choice selection
    + Object guessing
    
    ~ Objects can only be guessed one time per appearance
    ~ This is a 2D test version of the game to see if it's better in 2D.
    
    ! Unable to do multiple trials
    ! Unable to select number of trials
    ! No scoring yet.

---
+++ Reflex-Check-2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] possibleMemoryObjects;
+
+	private GameObject targetObject;
+	private GameObject[] memoryObjectChoices;
+	private int index;
+
+	private float targetInstanceNumber;
+
+	private float spawnTimer = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+				List<GameObject> possibleChoices = new List<GameObject> ();
+				possibleChoices.AddRange (possibleMemoryObjects);
+
+				int randomObject = Random.Range (0, possibleChoices.Count - 1);
+
+				targetObject = (GameObject)Instantiate (possibleChoices [randomObject]);
+
+				MemoryObjectController targetControl = targetObject.GetComponent<MemoryObjectController> ();
+				targetInstanceNumber = targetControl.getInstanceNumber ();
+				targetControl.turnOffGuess ();
+
+				targetObject.SetActive (false);
+
+				possibleChoices.RemoveAt (randomObject);
+
+				memoryObjectChoices = new GameObject[6];
+
+				float targetPosition = Random.Range (0, 6);
+
+				for (int i = 0; i<6; i++) {
+						if (i == (int)targetPosition) {
+								memoryObjectChoices [i] = targetObject;
+						} else {
+								randomObject = Random.Range (0, possibleChoices.Count - 1);
+
+								GameObject chosenObject = (GameObject)Instantiate (possibleChoices [randomObject]);
+								chosenObject.SetActive (false);
+
+								memoryObjectChoices [i] = chosenObject;
+								possibleChoices.RemoveAt (randomObject);
+						}
+				}
+
+				index = 0;
+
+				targetObject.SetActive (true);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+				spawnTimer -= Time.deltaTime;
+
+				if (spawnTimer <= 0 && index < memoryObjectChoices.Length) {
+						spawnTimer = 3.0f;
+						memoryObjectChoices [index].SetActive (true);
+						index++;
+				}
+		}
+
+	/**
+	 * Called by the Memory Object when one is selected.
+	 * Checks to see if the instance number of the choice
+	 * matches the instance number of the target.
+	 * **/
+	public void notifyGuess (float instanceNumber) {
+				Debug.Log ("Your guess was: " + (instanceNumber == targetInstanceNumber ? "Correct!" : "Incorrect"));
+		}
+}
---
+++ Reflex-Check-2D/Assets/Scripts/MemoryObjectController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Class MemoryObjectController extends MonoBehaviour
+ * 
+ * Used in: All MemoryObject prefabs
+ * 
+ * private const float TimeLimit - time limit for object to be on screen.
+ * private float timeRemaining - time remaining for the object to be on screen.
+ * private bool objectGuessed - determines whether or not the object has been guessed as a match.
+ * private GameController gameControl - allows the object to communicate with the game controller.
+ * private float instanceNumber - used for instance identification (to see if the guess was correct)
+ * 
+ * Overview: At creation, sets the timeRemaining to the max time limit, initializes the gameControl if null,
+ * and initializes the objectGuessed variable to false - i.e., the object has not been guessed yet.
+ * Each call of the Update function, checks to see the object has been guessed and acts accordingly.
+ * In addition, decrements the time remaining and checks to see if time is up. If time is up, destroys the object.
+ * **/
+public class MemoryObjectController : MonoBehaviour {
+	
+	private const float TimeLimit = 2.0f;
+	private float timeRemaining;
+
+	private bool objectGuessed;
+	
+	private GameController gameControl;
+
+	private float instanceNumber;
+
+	// Use this for initialization
+	void Awake () {
+				timeRemaining = TimeLimit;	
+
+				objectGuessed = false;
+
+				gameControl = GameObject.Find ("_GameController").GetComponent<GameController> ();
+
+				instanceNumber = Random.Range (1000, 9999999);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+				if (!objectGuessed && Input.GetKeyDown (KeyCode.Space)) {
+						objectGuessed = true;
+						gameControl.notifyGuess (instanceNumber);
+				}
+
+				timeRemaining -= Time.deltaTime;
+
+				if (timeRemaining <= 0.0f) {
+			timeRemaining = 2.0f;
+			objectGuessed = false;
+						gameObject.SetActive (false);
+				}
+		}
+
+	/**
+	 * Manually sets the instance number.
+	 * **/
+	public void setInstanceNumber (float idNum) {
+				instanceNumber = idNum;
+		}
+
+	/**
+	 * Gets the instance number.
+	 * **/
+	public float getInstanceNumber () {
+				return instanceNumber;
+		}
+
+	/**
+	 * Manually turns off the ability for this object to be guessed
+	 * **/
+	public void turnOffGuess(){
+				objectGuessed = true;
+		}
+
+	/**
+	 * Manually turns on the ability for this object to be guessed
+	 * **/
+	public void turnOnGuess () {
+				objectGuessed = false;
+		}
+}
